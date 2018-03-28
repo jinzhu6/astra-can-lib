446bd42e7b84c117ab428f3a7dfae629045f01ed
Drift3r <megadrifter@gmail.com>
2018-03-28 09:30:28 +0300

    ==

--- astra-001.ino
+++ astra-001.ino
@@ -3,17 +3,16 @@
 checkLsTriggers(){
 	int id = inMsg->ID;
 	
-	if (id=0x170){
+	if (id==0x170){
 		// buttons on key holder
 		if (inMsg->Data[0]==0x00 and inMsg->Data[1]==0x00) {
 			// some logic here 
 			ms.sendMessage(0x100,2,0x00,0x01);
 		}
 	}
-	else if (id = 160){
+	else if (id == 160){
 		//something else
 		ls.sendMessage(0x100,0);
 		ls.showEcn(123456);
 	}
 }
-
