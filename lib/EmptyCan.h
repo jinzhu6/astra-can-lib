f0fbb7c41eceb96343dfbc18793dc22e3a744336
Drift3r <megadrifter@gmail.com>
2018-05-08 23:49:10 +0300

    Компилируется, запускается, шлёт дебаг, принимает сообщенияgit status

--- lib/EmptyCan.h
+++ lib/EmptyCan.h
@@ -5,17 +5,16 @@
 #include "usb_serial.h"
 #include <HardwareCAN.h>
 
-class EmptyCan:HardwareCAN {
+class EmptyCan : public HardwareCAN {
 	private:
-	
 	public:
 	
+
+	EmptyCan(void):HardwareCAN(CAN1_BASE){
+		
+
+	}
 	
-	EmptyCan(void);
-	
-	void begin(void);
-	
-	void sendMessage(0x5e8,4,0x81, d0, d1, d2,0,0,0,0);
-	
-	
-}
+};
+
+#endif
